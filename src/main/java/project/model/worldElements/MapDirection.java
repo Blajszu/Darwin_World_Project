c6b89d707f483eaa8f049131817a6b90a6d67cec
Blajszu <oskar.blajsz@gmail.com>
2024-12-31 14:16:18 +0100

    Add worldElements code from Laboratories

---
+++ src/main/java/project/model/worldElements/MapDirection.java
@@ -0,0 +1,38 @@
+package project.model.worldElements;
+
+import project.model.Vector2d;
+
+public enum MapDirection {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
+
+    private static final Vector2d[] directions = {
+            new Vector2d(0, 1),
+            new Vector2d(1, 0),
+            new Vector2d(0, -1),
+            new Vector2d(-1, 0)
+    };
+
+    private static final String[] names = {"Polnoc", "Wschod", "Poludnie", "Zachod"};
+
+    @Override
+    public String toString() {
+        return names[this.ordinal()];
+    }
+
+    public MapDirection next() {
+        return MapDirection.values()[((this.ordinal()+1)%4)];
+    }
+
+    public MapDirection previous() {
+        int index = this.ordinal()-1;
+        if (index < 0) index = 3;
+        return MapDirection.values()[index];
+    }
+
+    public Vector2d toUnitVector () {
+        return directions[this.ordinal()];
+    }
+}
---
+++ src/main/java/project/model/worldElements/WorldElement.java
@@ -0,0 +1,12 @@
+package project.model.worldElements;
+
+import project.model.Vector2d;
+
+public interface WorldElement {
+
+    Vector2d getPosition();
+
+    String getResourceName();
+
+    String getResourceFileName();
+}
