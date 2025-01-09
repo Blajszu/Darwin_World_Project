4c51ae48d7869e2cfbbee241688c2c80c5c4f473
Blajszu <oskar.blajsz@gmail.com>
2025-01-09 22:36:37 +0100

    Add Main Module in Simulation

---
+++ src/main/java/project/GrowthGrassVariant.java
@@ -0,0 +1,6 @@
+package project;
+
+public enum GrowthGrassVariant {
+    EQUATOR_MAP,
+    MOVING_JUNGLE_MAP
+}
--- src/main/java/project/Simulation.java
+++ src/main/java/project/Simulation.java
@@ -1,73 +1,84 @@
 package project;
 
-import project.model.Vector2d;
-import project.model.maps.IncorrectPositionException;
+import project.model.maps.EquatorMap;
+import project.model.maps.MovingJungleMap;
 import project.model.maps.WorldMap;
-import project.model.worldElements.Animal;
-import project.model.worldElements.MapDirection;
-
-import java.util.ArrayList;
-import java.util.List;
+import project.model.worldElements.*;
 
 public class Simulation implements Runnable {
 
-    private final List<Animal> animals;
-    private final List<MoveDirection> moves;
     private final WorldMap worldMap;
+    private final int energyFromGrass;
+    private final int numberOfGrassGrowingEveryDay;
+    private final boolean collectStatistics;
 
-    public Simulation(List<Vector2d> animalPositions, List<MoveDirection> moveDirections, WorldMap map) {
-        moves = moveDirections;
-        animals = new ArrayList<>();
-        worldMap = map;
+    public Simulation(
+            int mapHeight,
+            int mapWith,
+            GrowthGrassVariant growthVariant,
+            int startNumberOfGrass,
+            int energyFromGrass,
+            int numberOfGrassGrowingEveryDay,
+            int startNumberOfAnimals,
+            int initialAnimalsEnergy,
+            int energyNeedToReproduce,
+            int energyUsedToReproduce,
+            int minimalNumberOfMutation,
+            int maximumNumberOfMutation,
+            MutationVariant mutationVariant,
+            int numberOfGenes,
+            boolean collectStatistics) {
 
-        for(Vector2d position : animalPositions) {
-            Animal newAnimal = new Animal(position);
-
-            try {
-                map.place(newAnimal);
-                animals.add(newAnimal);
-            }
-            catch (IncorrectPositionException e) {
-                e.printStackTrace(System.err);
-            }
+        if(
+                mapHeight <= 0 ||
+                mapWith <= 0 ||
+                startNumberOfGrass < 0 ||
+                energyFromGrass <= 0 ||
+                numberOfGrassGrowingEveryDay <= 0 ||
+                startNumberOfAnimals < 0 ||
+                initialAnimalsEnergy <= 0 ||
+                energyNeedToReproduce < 0 ||
+                energyUsedToReproduce < 0 ||
+                minimalNumberOfMutation < 0 ||
+                maximumNumberOfMutation < 0 ||
+                numberOfGenes <= 0) {
+            throw new IllegalArgumentException("Invalid Simulation parameters");
         }
+
+        worldMap = switch (growthVariant) {
+            case EQUATOR_MAP -> new EquatorMap(mapHeight, mapWith);
+            case MOVING_JUNGLE_MAP -> new MovingJungleMap(mapHeight, mapWith);
+        };
+
+        this.energyFromGrass = energyFromGrass;
+        this.numberOfGrassGrowingEveryDay = numberOfGrassGrowingEveryDay;
+        this.collectStatistics = collectStatistics;
+
+        MutationStrategy mutationStrategy = switch (mutationVariant) {
+            case RANDOM -> new RandomMutationStrategyVariant(minimalNumberOfMutation, maximumNumberOfMutation);
+            case INCREMENT_DECREMENT -> new IncrementDecrementMutationStrategyVariant(minimalNumberOfMutation, maximumNumberOfMutation);
+        };
+
+        spawnFirstAnimals(startNumberOfAnimals, initialAnimalsEnergy, energyNeedToReproduce, energyUsedToReproduce, mutationStrategy, numberOfGenes);
+        spawnGrass(startNumberOfGrass);
     }
 
-    List<Vector2d> getAnimalsPositionsAfterSimulation() {
-        List<Vector2d> positions = new ArrayList<>();
-        for(Animal animal : animals) {
-            positions.add(animal.getPosition());
-        }
+    private void spawnFirstAnimals(
+            int numberOfAnimalsToSpawn,
+            int initialAnimalsEnergy,
+            int energyNeedToReproduce,
+            int energyUsedToReproduce,
+            MutationStrategy mutationStrategy,
+            int numberOfGenes) {
 
-        return positions;
     }
 
-    List<MapDirection> getAnimalOrientationsAfterSimulation() {
-        List<MapDirection> orientations = new ArrayList<>();
-        for(Animal animal : animals) {
-            orientations.add(animal.getCurrentOrientation());
-        }
+    private void spawnGrass(int numberOfGrassToSpawn) {
 
-        return orientations;
     }
 
+    @Override
     public void run() {
-        int numberOfAnimals = animals.size();
-        int numberOfMoves = moves.size();
 
-        if(numberOfAnimals == 0)
-            return;
-
-        try {
-            for (int i = 0; i < numberOfMoves; i++) {
-                Animal currentAnimal = animals.get(i % numberOfAnimals);
-
-                worldMap.move(currentAnimal, moves.get(i));
-                Thread.sleep(500);
-            }
-        }
-        catch (InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
