17d35149bbb1408e01f02f112e7e8bb6c83f44bc
Blajszu <oskar.blajsz@gmail.com>
2025-01-12 01:10:20 +0100

    change method removeDeadAnimals from public to private

--- src/main/java/project/Simulation.java
+++ src/main/java/project/Simulation.java
@@ -84,7 +84,7 @@ public class Simulation implements Runnable {
 
     }
 
-    public void removeDeadAnimals() {
+    private void removeDeadAnimals() {
         Collection<Animal> allAnimalsOnMap = worldMap.getOrderedAnimals();
         for (Animal animal : allAnimalsOnMap) {
             if(!animal.isAnimalAlive()){
